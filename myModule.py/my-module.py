95eb37f3bf49b479dfc5f3d08d3fd89fd9a20e71
maryamfaizan53 <maryam.faizanmianoor@gmail.com>
2025-04-07 01:26:59 +0500

    done

---
+++ myModule.py/my-module.py
@@ -0,0 +1,13 @@
+#1.standard library modules
+import math
+print(math.sqrt(25))  # Output: 5.0
+
+
+#2. Built-in Modules
+import add
+print(mymodule.add(5, 3)
+      
+# 3. External Modules (Third-party Libraries)
+# Installed via pip (pip install module_name).
+# Example: numpy, pandas, requests
+# Example usage:      
